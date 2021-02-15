d051fbbbf078ee9e5c383884a65fb77e09f0f766
Piotr Paul <piotr.paul@gmail.com>
2021-02-15 11:26:28 +0100

    Prace programistyczne luty 2021

---
+++ src/TaxationAndCustomsUnion/Vies.Core.RazorClassLibrary/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,15 @@
+﻿#region using
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+#endregion
+
+namespace Vies.Core.RazorClassLibrary.MyFeature.Pages
+{
+    public class Page1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ src/TaxationAndCustomsUnion/Vies.Core.WebApp/WeatherForecast.cs
@@ -0,0 +1,19 @@
+#region using
+
+using System;
+
+#endregion
+
+namespace Vies.Core.WebApp
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
